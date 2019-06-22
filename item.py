77a25b6a1ac84b797c88faf5c03e4987fa67b3cd
Yuliya Brechka <juliabrechka@gmail.com>
2019-06-22 15:46:16 +0300

    improve methods

--- item.py
+++ item.py
@@ -72,15 +72,43 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = Item.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                Item.insert(updated_item)
+            except:
+                {"message": "An error occurred inserting te item."}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                Item.update(updated_item)
+            except:
+                {"message": "An error occurred updating te item."}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
         return {'items': items}
