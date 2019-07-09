026f7965b16483b51653db34803af25952885c6f
Yuliya Brechka <juliabrechka@gmail.com>
2019-07-09 19:18:18 +0300

    add func delete_from_db for User, add class for retrieving user's details and delete users, update the code

--- models/user.py
+++ models/user.py
@@ -1,26 +1,35 @@
-import sqlite3
 from db import db
 
 
-class UserModel(db.Model):                            # API
+class UserModel(db.Model):                                  # API
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))               # 80 characters max
+    username = db.Column(db.String(80))                     # 80 characters max
     password = db.Column(db.String(80))
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
     def save_to_db(self):
-        db.session.add(self)                    # session - collection of objects for the db
+        db.session.add(self)                                # session - collection of objects for the db
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
         db.session.commit()
 
     @classmethod
-    def find_by_username(cls, username):                        # searching by username
-        cls.query.filter_by(username=username).first()          # SELECT * FROM users
+    def find_by_username(cls, username):                           # searching by username
+        return cls.query.filter_by(username=username).first()      # SELECT * FROM users
 
     @classmethod
     def find_by_id(cls, _id):
-        cls.query.filter_by(id=_id).first()
+        return cls.query.filter_by(id=_id).first()
--- resources/user.py
+++ resources/user.py
@@ -1,29 +1,48 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
 
 class UserRegister(Resource):                                   # allows users to sign up
-
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field couldn't be blank"
-    )
+                        type=str,
+                        required=True,
+                        help="This field couldn't be blank"
+                        )
     parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field couldn't be blank"
-    )
+                        type=str,
+                        required=True,
+                        help="This field couldn't be blank"
+                        )
 
     def post(self):
         data = UserRegister.parser.parse_args()
 
         if UserModel.find_by_username(data['username']):             # preventing duplicate usernames
-            return {"message": "A user with that username already exists."}, 400
+            return {'message': 'A user with that username already exists.'}, 400
 
         user = UserModel(**data)
         user.save_to_db()
 
-        return {"message": "User created successfully."}, 201
+        return {'message': 'User created successfully.'}, 201
+
+
+class User(Resource):
+    """
+    Retrieve user's details and delete users.
+    """
+
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found.'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted.'}, 200
