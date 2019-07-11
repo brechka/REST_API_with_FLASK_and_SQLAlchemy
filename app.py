0cc2e0077e711d4e766aef9e12fff96b7fc4b61f
Yuliya Brechka <juliabrechka@gmail.com>
2019-07-11 18:54:44 +0300

    customize callbacks and responses

--- app.py
+++ app.py
@@ -34,6 +34,45 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+@jwt.expired_token_loader
+# notify about access_token expiring; ask to authenticate again
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+# sended token in authorization header is not jwt
+def invalid_token_callback(error):
+    return jsonofy({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+# don't send a jwt at all
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+@jwt.needs_fresh_token_loader
+# send a Non fresh token, but required a fresh one
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader
+# revoke a token = this token is no longer valid; used for logging out a user
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
