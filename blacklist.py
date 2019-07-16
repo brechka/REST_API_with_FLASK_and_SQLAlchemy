de816be3409c09659a135115c10484a89b4a15bd
Yuliya Brechka <juliabrechka@gmail.com>
2019-07-16 15:47:05 +0300

    add a commet

--- blacklist.py
+++ blacklist.py
@@ -1,1 +1,8 @@
+"""
+This file just contains the blacklist of the JWT tokens–it will be imported by
+app and the logout resource. So that tokens can be added to the blacklist when the
+user logs out.
+"""
+
+
 BLACKLIST = set()
